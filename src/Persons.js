5a13db43bd165c5ff8b53520a929fc96e48781bf
eeva.alanko <eeva.alanko@siili.com>
2023-03-25 00:17:47 +0200

    fix test

--- src/Persons.js
+++ src/Persons.js
@@ -1,17 +1,20 @@
-import React from 'react'
+import React from 'react';
 
-const Persons = ({ persons, removePerson }) => (
-  <div>
-    <h2>Numbers</h2>
-    {persons.map((person) => (
-      <p key={person.name}>
-        {person.name} {person.number}{' '}
-        <button type="text" onClick={() => removePerson(person)}>
-          delete
-        </button>
-      </p>
-    ))}
-  </div>
-)
+const Persons = ({ persons, removePerson }) => {
+  console.log('persons, persons', persons);
+  return (
+    <div>
+      <h2>Numbers</h2>
+      {persons.map(person => (
+        <p key={person.name}>
+          {person.name} {person.number}{' '}
+          <button type="text" onClick={() => removePerson(person)}>
+            delete
+          </button>
+        </p>
+      ))}
+    </div>
+  );
+};
 
-export default Persons
+export default Persons;
