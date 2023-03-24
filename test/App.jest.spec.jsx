7b5ef4e7eac5bfa87aa7a5c41619f412a2e84050
eeva.alanko <eeva.alanko@siili.com>
2023-03-25 00:29:44 +0200

    fix test

--- test/App.jest.spec.jsx
+++ test/App.jest.spec.jsx
@@ -1,19 +1,19 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import axiosMock from 'axios';
-import { act } from 'react-dom/test-utils';
-import '@testing-library/jest-dom/extend-expect';
-import App from '../src/App';
+import React from 'react'
+import { render } from '@testing-library/react'
+import axiosMock from 'axios'
+import { act } from 'react-dom/test-utils'
+import '@testing-library/jest-dom/extend-expect'
+import App from '../src/App'
 
-jest.mock('axios');
+jest.mock('axios')
 
 describe('<App />', () => {
   it('fetches data', async () => {
     axiosMock.get.mockResolvedValueOnce({
       data: [{ url: 'http://localhost:3001/persons/1', name: 'Loid Forger', id: 1, number: '1234' }],
-    });
+    })
     await act(async () => {
-      render(<App />);
-    });
-  });
-});
+      render(<App />)
+    })
+  })
+})
